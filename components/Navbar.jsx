09b25b8795014a3e30fb92edaa1949bec585fe73
Hamza <mhamza@sentinel-ai.io>
2024-05-19 01:06:07 +0500

    feat: Add hamburger menu icon to Navbar component and make it responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,32 +4,46 @@ import React, { useState } from "react";
 
 import { CiLocationOn } from "react-icons/ci";
 import { IoMdSearch } from "react-icons/io";
-import { FaRegUser } from "react-icons/fa6";
+import { FaRegUser, FaBars } from "react-icons/fa6";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
   const [showSearchBox, setShowSearchBox] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <>
       <div className="w-full z-10">
         <nav
           style={{ backgroundColor: "#d35400", border: "#d35400" }}
-          className="flex items-center w-full px-4 py-8 text-black rounded-none shadow-md h-max bg-opacity-80 lg:px-8 lg:py-4 justify-between mx-auto text-blue-gray-900"
+          className="flex flex-col md:flex-row items-center w-full p-2 text-black rounded-none shadow-md bg-opacity-80 justify-between mx-auto text-blue-gray-900"
         >
-          <a className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
-            <Image
-              src="/logo-without-text.png"
-              width={60}
-              height={60}
-              objectFit="contain"
-              alt="Logo"
-            />
-          </a>
-          <div className="hidden lg:block">
-            <ul className="flex flex-col float-right gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+          <div className="w-full flex items-center justify-between flex-1">
+            <a className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
+              <Image
+                src="/logo-without-text.png"
+                width={60}
+                height={60}
+                objectFit="contain"
+                alt="Logo"
+              />
+            </a>
+
+            {/* Hamburger icon that's visible only till md screen and then hidden */}
+            <div className="block md:hidden">
+              <button
+                className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2 text-blue-gray-900"
+                onClick={() => setShowMenu(!showMenu)}
+              >
+                <FaBars style={{ fontSize: "25px", color: "#ecf0f1" }} />
+              </button>
+            </div>
+          </div>
+
+          <div className={`${!showMenu && "hidden"} md:block`}>
+            <ul className="flex gap-4 flex-col justify-center items-center md:flex-row">
               <li className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2 text-blue-gray-900 overflow-hidden">
                 <form>
                   <label
@@ -41,19 +55,19 @@ export default function Navbar() {
                   <div className="relative">
                     <div
                       className={`${
-                        showSearchBox ? "" : "opacity-0 scale-x-0"
+                        showSearchBox || showMenu ? "" : "opacity-0 scale-x-0"
                       } transition-all duration-300 origin-right`}
                     >
                       <input
                         type="search"
                         id="search"
-                        className="block bg-transparent focus:bg-transparent  w-full p-4 text-sm text-white rounded-lg placeholder-gray-200 dark:text-white"
+                        className="block bg-transparent focus:bg-transparent  w-full px-4 py-2 text-sm text-white rounded-lg placeholder-gray-200 dark:text-white border md:border-0"
                         placeholder="Search"
                         required
                       />
                     </div>
 
-                    <div className="absolute inset-y-0 end-0 flex items-center">
+                    <div className="absolute inset-y-0 end-0 md:flex items-center hidden">
                       <a
                         href="#"
                         className="flex items-center"
