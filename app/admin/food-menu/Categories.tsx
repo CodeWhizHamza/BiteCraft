cd779b7c1959fe3a425108f4dbf5c6ebe48593b1
Hamza <mhamza@sentinel-ai.io>
2024-05-19 21:08:42 +0500

    refactor: Update Categories component to include authentication token in API request

--- app/admin/food-menu/Categories.tsx
+++ app/admin/food-menu/Categories.tsx
@@ -4,14 +4,47 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useUserAuth } from "@/store/userAuth";
 
+interface Category {
+  _id: string;
+  name: string;
+}
+
 export default function Categories() {
   const token = useUserAuth((state) => state.authToken);
+  const [isAddingCategory, setIsAddingCategory] = useState(false);
+  const [editingCategoryId, setEditingCategoryId] = useState<string>("");
   const [showModal, setShowModal] = useState(false);
   const [title, setTitle] = useState("");
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [previousValue, setPreviousValue] = useState<string>("");
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await axios.get("/admin/food-menu/categories", {
+          headers: {
+            "auth-token": token,
+          },
+        });
+        const data = await response.data;
+        setCategories(data.data);
+      } catch (error: any) {
+        if (error.response) {
+          const data = await error.response.data;
+          toast.error(data.message);
+        } else {
+          toast.error("An error occurred. Please try again later.");
+        }
+      }
+    };
+
+    fetchCategories();
+  }, [token]);
 
   const handleAddCategoryClicked = () => {
     setTitle("Add New Category");
     setShowModal(true);
+    setIsAddingCategory(true);
   };
 
   const handleAddCategoryFormSubmit = async (
@@ -26,7 +59,7 @@ export default function Categories() {
 
     try {
       const response = await axios.post(
-        "/admin/food-menu/category",
+        "/admin/food-menu/categories",
         {
           name: formData.get("categoryName"),
         },
@@ -39,6 +72,11 @@ export default function Categories() {
       const data = await response.data;
       toast.success(data.message);
 
+      setCategories((prevCategories) => [
+        ...prevCategories,
+        { _id: data.data._id, name: data.data.name },
+      ]);
+
       setSpinning(false);
       setShowModal(false);
     } catch (error: any) {
@@ -50,6 +88,67 @@ export default function Categories() {
       }
       setSpinning(false);
     }
+  };
+
+  const handleEditCategoryClicked = (id: string) => {
+    setTitle("Edit Category");
+    setPreviousValue(
+      categories.find((category) => category._id === id)?.name as string
+    );
+    setIsAddingCategory(false);
+    setEditingCategoryId(id);
+    setShowModal(true);
+  };
+
+  const handleEditCategorySubmit = async (
+    e: React.FormEvent<HTMLFormElement>,
+    setSpinning: (value: boolean) => void
+  ) => {
+    e.preventDefault();
+    setSpinning(true);
+
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+
+    try {
+      const response = await axios.put(
+        `/admin/food-menu/categories/${editingCategoryId}`,
+        {
+          name: formData.get("categoryName"),
+        },
+        {
+          headers: {
+            "auth-token": token,
+          },
+        }
+      );
+      const data = await response.data;
+      toast.success(data.message);
+
+      setCategories((prevCategories) =>
+        prevCategories.map((category) =>
+          category._id === editingCategoryId
+            ? { ...category, name: formData.get("categoryName") as string }
+            : category
+        )
+      );
+
+      setSpinning(false);
+      setShowModal(false);
+    } catch (error: any) {
+      if (error.response) {
+        const data = await error.response.data;
+        toast.error(data.message);
+      } else {
+        toast.error("An error occurred. Please try again later.");
+      }
+    }
+
+    setSpinning(false);
+  };
+
+  const handleDeleteCategoryClicked = (id: string) => {
+    console.log("Delete category with id: ", id);
   };
 
   return (
@@ -82,23 +181,34 @@ export default function Categories() {
               </tr>
             </thead>
             <tbody>
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              {categories.map((category) => (
+                <tr
+                  key={category._id}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
                 >
-                  Apple MacBook Pro 17&quot;
-                </th>
-                <td className="px-6 py-4">Silver</td>
-                <td className="px-6 py-4 text-right flex gap-4 justify-end">
-                  <button className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
-                    Edit
-                  </button>
-                  <button className="font-medium text-red-600 dark:text-red-500 hover:underline">
-                    Delete
-                  </button>
-                </td>
-              </tr>
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {category.name}
+                  </th>
+                  <td className="px-6 py-4">5</td>
+                  <td className="px-6 py-4 text-right flex gap-4 justify-end">
+                    <button
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                      onClick={() => handleEditCategoryClicked(category._id)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="font-medium text-red-600 dark:text-red-500 hover:underline"
+                      onClick={() => handleDeleteCategoryClicked(category._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
@@ -108,7 +218,12 @@ export default function Categories() {
         show={showModal}
         setShow={setShowModal}
         title={title}
-        onSubmit={handleAddCategoryFormSubmit}
+        onSubmit={
+          isAddingCategory
+            ? handleAddCategoryFormSubmit
+            : handleEditCategorySubmit
+        }
+        categoryDefaultValue={previousValue}
       />
     </>
   );
