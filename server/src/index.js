58eaab86dcf33ddc02ac13a59d33670f8512f908
Hamza <mhamza@sentinel-ai.io>
2024-05-19 20:19:57 +0500

    refactor: Update index.js to use new middleware for authentication

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const connectToDB = require("./helpers/connectToDB");
-const verifyToken = require("./middlewares/auth");
+const verifyToken = require("./middleware/auth");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
