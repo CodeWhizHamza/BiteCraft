f011fabd7c6d11590cc85b2318f06fe3fff64415
Hamza <mhamza@sentinel-ai.io>
2024-05-19 20:16:06 +0500

    feat: Add screen height utility class to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      height: {
+        screen: "100vh",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
