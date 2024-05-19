4fec7f1ccc2583524afb480cd5e40f79801bf429
Hamza <mhamza@sentinel-ai.io>
2024-05-19 13:37:29 +0500

    refactor: Improve user model validation and add createdAt field

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -3,11 +3,19 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Name is required"],
+    trim: true,
   },
   phoneNumber: {
     type: String,
-    required: true,
+    required: [true, "Phone number is required"],
+    unique: [true, "Phone number already exists"],
+    validate: {
+      validator(v) {
+        return /\d{11,13}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
   },
   password: {
     type: String,
@@ -15,10 +23,17 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    required: true,
+    required: [true, "Role is required"],
+    enum: ["admin", "user", "deliveryboy"],
   },
   address: {
     type: String,
     required: true,
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+exports.default = mongoose.model("User", userSchema);
