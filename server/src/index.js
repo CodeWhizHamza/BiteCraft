898db234d05e8c8e0e278a0f8eff8c1e6590ca1e
Hamza <mhamza@sentinel-ai.io>
2024-05-19 13:37:23 +0500

    refactor: Update server index.js for improved routing and database connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+require("dotenv").config();
+const connectToDB = require("./helpers/connectToDB");
+const userRouter = require("./routes/user");
+
 const app = express();
-const port = 5500;
+const port = process.env.PORT || 5050;
+
+connectToDB();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
