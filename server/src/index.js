cc1c94f722ae4b50393d623ade5926d19d90e19d
Hamza <mhamza@sentinel-ai.io>
2024-05-19 15:23:34 +0500

    refactor: Add CORS headers and logger middleware to server index.js

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,18 @@ connectToDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+// logger middleware
+app.use((req, _, next) => {
+  console.log(`Request ${req.method} at ${req.url}`);
+  next();
+});
 
 app.use("/user", userRouter);
 
