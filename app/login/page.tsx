0dce716ede2242b5c85beb88650ff3d0bb5d979f
Hamza <mhamza@sentinel-ai.io>
2024-05-19 15:57:37 +0500

    refactor: Remove hardcoded values for phone and password fields in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -72,6 +72,7 @@ export default function Page() {
                 type="tel"
                 name="phone"
                 id="phone"
+                value="03421798786" // Comment it out after testing
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary2 focus:border-primary2 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                 placeholder="e.g. 0312 345 6789"
                 required
@@ -88,6 +89,7 @@ export default function Page() {
                 type="password"
                 name="password"
                 id="password"
+                value="12345678" // Comment it out after testing
                 placeholder="••••••••"
                 className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary2 focus:border-primary2 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                 required
