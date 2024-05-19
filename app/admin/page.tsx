55af33588ae4cb2792c2be666437a6acea801c10
Hamza <mhamza@sentinel-ai.io>
2024-05-19 20:17:14 +0500

    refactor: Update admin page to redirect to dashboard for non-admin users

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useUserAuth } from "@/store/userAuth";
 import { redirect, useRouter } from "next/navigation";
 
@@ -7,11 +6,6 @@ export default function Page() {
   const router = useRouter();
   const isLoggedIn = useUserAuth((state) => state.isLoggedIn);
   const isNotAdmin = useUserAuth((state) => state.user?.role !== "admin");
-  const logout = useUserAuth((state) => state.logout);
-
-  const handleLogout = () => {
-    logout();
-  };
 
   if (!isLoggedIn) {
     router.replace("/admin/login");
@@ -19,19 +13,8 @@ export default function Page() {
   }
 
   if (isNotAdmin) {
-    router.replace("/");
+    router.replace("/dashboard");
     return null;
   }
-
-  return (
-    <div>
-      <h1>Admin Login</h1>
-      <button
-        onClick={handleLogout}
-        className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded mt-4"
-      >
-        Logout
-      </button>
-    </div>
-  );
+  return redirect("/admin/food-menu");
 }
