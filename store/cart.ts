4d59fcf1c82649d8802e7d82542ea94f6fb4a147
Hamza <mhamza@sentinel-ai.io>
2024-05-20 13:57:51 +0500

    refactor: Add persist middleware to useCartStore for persistent cart state storage

--- store/cart.ts
+++ store/cart.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface ICartStore {
   cart: {
@@ -10,28 +11,41 @@ interface ICartStore {
   updateQuantity: (id: string, quantity: number) => void;
 }
 
-export const useCartStore = create<ICartStore>((set) => ({
-  cart: [],
-  addToCart: (id: string) =>
-    set((state) => {
-      const item = state.cart.find((item) => item.id === id);
-      if (item) {
-        return {
+export const useCartStore = create(
+  persist<ICartStore>(
+    (set) => ({
+      cart: [],
+      addToCart: (id: string) => {
+        set((state) => {
+          const item = state.cart.find((item) => item.id === id);
+          if (item) {
+            return {
+              cart: state.cart.map((item) =>
+                item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+              ),
+            };
+          } else {
+            return {
+              cart: [...state.cart, { id, quantity: 1 }],
+            };
+          }
+        });
+      },
+      removeFromCart: (id: string) => {
+        set((state) => ({
+          cart: state.cart.filter((item) => item.id !== id),
+        }));
+      },
+      updateQuantity: (id: string, quantity: number) => {
+        set((state) => ({
           cart: state.cart.map((item) =>
-            item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+            item.id === id ? { ...item, quantity } : item
           ),
-        };
-      }
-      return { cart: [...state.cart, { id, quantity: 1 }] };
+        }));
+      },
     }),
-  removeFromCart: (id: string) =>
-    set((state) => ({
-      cart: state.cart.filter((item) => item.id !== id),
-    })),
-  updateQuantity: (id: string, quantity: number) =>
-    set((state) => ({
-      cart: state.cart.map((item) =>
-        item.id === id ? { ...item, quantity } : item
-      ),
-    })),
-}));
+    {
+      name: "cart",
+    }
+  )
+);
