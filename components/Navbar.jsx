f28d29643e515ec79ecd6cce17f14485cd05a76b
Hamza <mhamza@sentinel-ai.io>
2024-05-20 13:46:11 +0500

    chore: Add cart item count badge to Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { CiLocationOn } from "react-icons/ci";
 import { IoMdSearch } from "react-icons/io";
@@ -8,10 +8,17 @@ import { FaRegUser, FaBars } from "react-icons/fa6";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import Image from "next/image";
 import Link from "next/link";
+import { useCartStore } from "@/store/cart";
 
 export default function Navbar() {
   const [showSearchBox, setShowSearchBox] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
+  const cart = useCartStore((state) => state.cart);
+  const [numberOfItems, setNumberOfItems] = useState(0);
+
+  useEffect(() => {
+    setNumberOfItems(cart.length);
+  }, [cart]);
 
   return (
     <>
@@ -89,7 +96,14 @@ export default function Navbar() {
                   <FaRegUser style={{ fontSize: "25px", color: "#ecf0f1" }} />
                 </Link>
               </li>
-              <li className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2 text-blue-gray-900">
+              <li className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2 text-blue-gray-900 relative">
+                {/* A red badge that shows the number of items in the cart */}
+                {numberOfItems > 0 ||
+                  (true && (
+                    <span className="absolute -top-2 -right-2 flex w-5 h-5 items-center justify-center bg-blue-500 text-white rounded-full p-1 text-xs leading-none">
+                      {numberOfItems > 9 ? "9+" : numberOfItems}
+                    </span>
+                  ))}
                 <Link className="flex items-center" href="/cart">
                   <AiOutlineShoppingCart
                     style={{ fontSize: "25px", color: "#ecf0f1" }}
