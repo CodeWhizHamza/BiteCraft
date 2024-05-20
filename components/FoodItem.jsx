c9ed0e615d7cd7c422a8219130b2e50dc338f21c
Hamza <mhamza@sentinel-ai.io>
2024-05-20 13:57:36 +0500

    refactor: Add addToCart functionality to FoodItem component

--- components/FoodItem.jsx
+++ components/FoodItem.jsx
@@ -1,11 +1,17 @@
+import { useCartStore } from "@/store/cart";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function FoodItem({ item }) {
   const [rating, setRating] = useState(0);
+  const addToCart = useCartStore((state) => state.addToCart);
 
   const handleStarClick = (value) => {
     setRating(value);
+  };
+
+  const handleAddToCart = (id) => {
+    addToCart(id);
   };
 
   return (
@@ -58,7 +64,10 @@ export default function FoodItem({ item }) {
           <span className="text-lg font-bold text-gray-900 dark:text-white">
             Rs. {item.price}
           </span>
-          <button className="text-white bg-primary hover:bg-primary3 focus:ring-0 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center  dark:hover:bg-primary dark:focus:ring-0">
+          <button
+            className="text-white bg-primary hover:bg-primary3 focus:ring-0 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center  dark:hover:bg-primary dark:focus:ring-0"
+            onClick={() => handleAddToCart(item._id)}
+          >
             Add To Cart
           </button>
         </div>
