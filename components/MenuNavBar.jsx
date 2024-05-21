36c8a516e518358029cca1faa8e94b754812640e
Hamza <muhammadhamzam1486@gmail.com>
2024-05-21 15:38:01 +0500

    chore: Update MenuNavBar component styling

--- components/MenuNavBar.jsx
+++ components/MenuNavBar.jsx
@@ -1,9 +1,6 @@
 export default function MenuNavbar({ foodTypes }) {
   return (
-    <nav
-      style={{ backgroundColor: "#f39c12", border: "#d35400" }}
-      className="block w-full max-w-full px-4 py-8 text-black bg-white border rounded-none shadow-md h-max  bg-opacity-80 lg:px-8 lg:py-4"
-    >
+    <nav className="bg-primary border-primary block w-full max-w-full px-4 py-8 text-black border rounded-none shadow-md h-max  bg-opacity-80 lg:px-8 lg:py-4">
       <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <ul className="flex flex-col float-right gap-2 mt-2 mb-4 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
           {foodTypes.map((item) => (
@@ -13,9 +10,8 @@ export default function MenuNavbar({ foodTypes }) {
             >
               <a className="flex items-center" href={item.id}>
                 <button
-                  style={{ backgroundColor: "#f1c40f" }}
                   type="button"
-                  className="text-white px-8  bg-blue-700 focus:ring-0 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-0"
+                  className="text-white px-8 focus:ring-0 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-0 bg-primary2"
                 >
                   {item.type}
                 </button>
