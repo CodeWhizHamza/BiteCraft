1fa0f668f878383cb7386acd47fd19ebacea7321
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-05-21 15:13:54 +0500

    refactor: Populate order items with product details

--- server/src/routes/order.js
+++ server/src/routes/order.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Order = require("../models/Order");
+const FoodItem = require("../models/FoodItem");
 const { verifyToken, isAdmin } = require("../middleware/auth");
 
 router.post("/", verifyToken, async (req, res) => {
@@ -27,20 +28,38 @@ router.post("/", verifyToken, async (req, res) => {
     }
 });
 
-router.get("/", verifyToken, isAdmin, async (req, res) => {
+router.get("/", verifyToken, async (req, res) => {
+
     try {
-        const orders = await Order.find().populate("user", "name email");
+        const orders = await Order.find({ user: req.user._id });
+        // order by created date
+        orders.sort((a, b) => b.createdAt - a.createdAt);
+
+        for (let i = 0; i < orders.length; i++) {
+
+            for (let j = 0; j < orders[i].items.length; j++) {
+                let item = orders[i].items[j];
+                let product = await FoodItem.findById(item.id);
+                orders[i].items[j]._doc.name = product.name;
+                orders[i].items[j]._doc.price = product.price;
+                orders[i].items[j]._doc.image = product.image;
+                orders[i].items[j]._doc.description = product.description;
+            }
+        }
         res.send({
             success: true,
             data: orders,
         });
-    } catch (error) {
+    }
+    catch (error) {
+        console.log(error);
         res.status(500).send({
             success: false,
             message: "Error fetching orders",
             error: error.message,
         });
     }
-});
+}
+);
 
 exports = module.exports = router;
