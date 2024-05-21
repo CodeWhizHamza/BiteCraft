d58bd9b9b4eae4d123f7dfeb77258f0906b88b8a
Hamza <muhammadhamzam1486@gmail.com>
2024-05-21 15:37:56 +0500

    refactor: Update HomeCookedGoodness component styling

--- components/HomeCookedGoodness.jsx
+++ components/HomeCookedGoodness.jsx
@@ -6,14 +6,8 @@ import AccordionWithCustomIcon from "./AccordionWithCustomIcon";
 export default function HomeCookedGoodness() {
   return (
     <>
-      <div
-        style={{ backgroundColor: "#f39c12" }}
-        className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2 text-blue-gray-900"
-      >
-        <div
-          style={{ backgroundColor: "#d35400", border: "#d35400" }}
-          className="mx-auto w-full max-w-screen-xl p-4 mt-12 py-6 lg:py-8  border-gray-200 shadow-2xl rounded-lg dark:bg-gray-800 dark:border-gray-700"
-        >
+      <div className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2 text-blue-gray-900 bg-primary2">
+        <div className="mx-auto w-full max-w-screen-xl p-4 mt-12 py-6 lg:py-8 shadow-2xl rounded-lg dark:bg-gray-800 dark:border-gray-700 bg-primary border-primary">
           <h2 className="text-2xl font-bold mb-4 text-center">
             Home Cooked Goodness
           </h2>
