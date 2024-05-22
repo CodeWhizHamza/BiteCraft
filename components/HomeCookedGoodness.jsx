f99da12ac6e80bd338336a469d9be5b6014ce876
Hamza <muhammadhamzam1486@gmail.com>
2024-05-22 09:59:06 +0500

    refactor: Update HomeCookedGoodness component to use consistent text color and add margin-bottom

--- components/HomeCookedGoodness.jsx
+++ components/HomeCookedGoodness.jsx
@@ -6,9 +6,9 @@ import AccordionWithCustomIcon from "./AccordionWithCustomIcon";
 export default function HomeCookedGoodness() {
   return (
     <>
-      <div className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2 text-blue-gray-900 bg-primary2">
+      <div className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2 text-blue-gray-900 mb-4">
         <div className="mx-auto w-full max-w-screen-xl p-4 mt-12 py-6 lg:py-8 shadow-2xl rounded-lg dark:bg-gray-800 dark:border-gray-700 bg-primary border-primary">
-          <h2 className="text-2xl font-bold mb-4 text-center">
+          <h2 className="text-2xl font-bold mb-4 text-center text-white">
             Home Cooked Goodness
           </h2>
           <AccordionWithCustomIcon />
